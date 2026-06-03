d5689c7323816fd1b6d466b744ce1afe95a30433
lincube <lincube3@hotmail.com>
2026-06-04 07:29:11 +0800

    Split FluentJalium navigation wrappers from control aggregate

--- src/FluentJalium/Controls/FWControls.cs
+++ src/FluentJalium/Controls/FWControls.cs
@@ -7,55 +7,6 @@ using FluentJalium.Icon;
 namespace FluentJalium.Controls;
 
 /// <summary>
-/// FluentJalium NavigationView control.
-/// </summary>
-public class FWNavigationView : NavigationView, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium NavigationViewItem control.
-/// </summary>
-public class FWNavigationViewItem : NavigationViewItem, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium NavigationViewItemHeader control.
-/// </summary>
-public class FWNavigationViewItemHeader : NavigationViewItemHeader, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium NavigationViewItemSeparator control.
-/// </summary>
-public class FWNavigationViewItemSeparator : NavigationViewItemSeparator, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium TabControl control.
-/// </summary>
-public class FWTabControl : TabControl, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium TabItem control.
-/// </summary>
-public class FWTabItem : TabItem, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium Frame control.
-/// </summary>
-public class FWFrame : Frame, IFluentJaliumControl
-{
-}
-
-/// <summary>
 /// FluentJalium Expander control.
 /// </summary>
 public class FWExpander : Expander, IFluentJaliumControl
---
+++ src/FluentJalium/Controls/Navigation/FWNavigationControls.cs
@@ -0,0 +1,52 @@
+using Jalium.UI.Controls;
+
+namespace FluentJalium.Controls;
+
+/// <summary>
+/// FluentJalium NavigationView control.
+/// </summary>
+public class FWNavigationView : NavigationView, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium NavigationViewItem control.
+/// </summary>
+public class FWNavigationViewItem : NavigationViewItem, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium NavigationViewItemHeader control.
+/// </summary>
+public class FWNavigationViewItemHeader : NavigationViewItemHeader, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium NavigationViewItemSeparator control.
+/// </summary>
+public class FWNavigationViewItemSeparator : NavigationViewItemSeparator, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium TabControl control.
+/// </summary>
+public class FWTabControl : TabControl, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium TabItem control.
+/// </summary>
+public class FWTabItem : TabItem, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium Frame control.
+/// </summary>
+public class FWFrame : Frame, IFluentJaliumControl
+{
+}
