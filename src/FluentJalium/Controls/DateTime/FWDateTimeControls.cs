a17998b0f0ef4a4a69ded4d0e78ad409f0161ce4
lincube <lincube3@hotmail.com>
2026-06-04 07:54:16 +0800

    Split FluentJalium date time wrappers from control aggregate

---
+++ src/FluentJalium/Controls/DateTime/FWDateTimeControls.cs
@@ -0,0 +1,24 @@
+using Jalium.UI.Controls;
+
+namespace FluentJalium.Controls;
+
+/// <summary>
+/// FluentJalium DatePicker control.
+/// </summary>
+public class FWDatePicker : DatePicker, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium TimePicker control.
+/// </summary>
+public class FWTimePicker : TimePicker, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium Calendar control.
+/// </summary>
+public class FWCalendar : Calendar, IFluentJaliumControl
+{
+}
--- src/FluentJalium/Controls/FWControls.cs
+++ src/FluentJalium/Controls/FWControls.cs
@@ -4,27 +4,6 @@ using Jalium.UI.Controls;
 namespace FluentJalium.Controls;
 
 /// <summary>
-/// FluentJalium DatePicker control.
-/// </summary>
-public class FWDatePicker : DatePicker, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium TimePicker control.
-/// </summary>
-public class FWTimePicker : TimePicker, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium Calendar control.
-/// </summary>
-public class FWCalendar : Calendar, IFluentJaliumControl
-{
-}
-
-/// <summary>
 /// FluentJalium InfoBar control.
 /// </summary>
 public class FWInfoBar : InfoBar, IFluentJaliumControl
