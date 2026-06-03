8eae2a5ca358dd5749612f2228feb6d851431424
lincube <lincube3@hotmail.com>
2026-06-04 07:32:28 +0800

    Split FluentJalium disclosure wrappers from control aggregate

---
+++ src/FluentJalium/Controls/Disclosure/FWDisclosureControls.cs
@@ -0,0 +1,31 @@
+using Jalium.UI.Controls;
+
+namespace FluentJalium.Controls;
+
+/// <summary>
+/// FluentJalium Expander control.
+/// </summary>
+public class FWExpander : Expander, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium ToolTip control.
+/// </summary>
+public class FWToolTip : ToolTip, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium ContentDialog control.
+/// </summary>
+public class FWContentDialog : ContentDialog, IFluentJaliumControl
+{
+}
+
+/// <summary>
+/// FluentJalium GroupBox control.
+/// </summary>
+public class FWGroupBox : GroupBox, IFluentJaliumControl
+{
+}
--- src/FluentJalium/Controls/FWControls.cs
+++ src/FluentJalium/Controls/FWControls.cs
@@ -7,34 +7,6 @@ using FluentJalium.Icon;
 namespace FluentJalium.Controls;
 
 /// <summary>
-/// FluentJalium Expander control.
-/// </summary>
-public class FWExpander : Expander, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium ToolTip control.
-/// </summary>
-public class FWToolTip : ToolTip, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium ContentDialog control.
-/// </summary>
-public class FWContentDialog : ContentDialog, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium GroupBox control.
-/// </summary>
-public class FWGroupBox : GroupBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
 /// FluentJalium ScrollViewer control.
 /// </summary>
 public class FWScrollViewer : ScrollViewer, IFluentJaliumControl
