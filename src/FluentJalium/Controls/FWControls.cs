86039de75ce0c38ed0371c9ab069cad76ad9b85f
lincube <lincube3@hotmail.com>
2026-06-04 07:49:41 +0800

    Split FluentJalium visual wrappers from control aggregate

--- src/FluentJalium/Controls/FWControls.cs
+++ src/FluentJalium/Controls/FWControls.cs
@@ -7,55 +7,6 @@ using FluentJalium.Icon;
 namespace FluentJalium.Controls;
 
 /// <summary>
-/// FluentJalium Image control.
-/// </summary>
-public class FWImage : Image, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium FontIcon control.
-/// </summary>
-public class FWFontIcon : FontIcon, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium SymbolIcon control.
-/// </summary>
-public class FWSymbolIcon : SymbolIcon, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium PathIcon control.
-/// </summary>
-public class FWPathIcon : PathIcon, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium Viewbox control.
-/// </summary>
-public class FWViewbox : Viewbox, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium Label control.
-/// </summary>
-public class FWLabel : Label, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium Separator control.
-/// </summary>
-public class FWSeparator : Separator, IFluentJaliumControl
-{
-}
-
-/// <summary>
 /// FluentJalium MenuBar control.
 /// </summary>
 public class FWMenuBar : MenuBar, IFluentJaliumControl
