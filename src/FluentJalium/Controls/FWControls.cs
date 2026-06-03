8732c1a2e4a2a97b596d8f39e96aa9e5ec8607b8
lincube <lincube3@hotmail.com>
2026-06-04 07:09:50 +0800

    Split FluentJalium text input wrappers from control aggregate

--- src/FluentJalium/Controls/FWControls.cs
+++ src/FluentJalium/Controls/FWControls.cs
@@ -7,41 +7,6 @@ using FluentJalium.Icon;
 namespace FluentJalium.Controls;
 
 /// <summary>
-/// FluentJalium TextBox control.
-/// </summary>
-public class FWTextBox : TextBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium PasswordBox control.
-/// </summary>
-public class FWPasswordBox : PasswordBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium NumberBox control.
-/// </summary>
-public class FWNumberBox : NumberBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium AutoCompleteBox control.
-/// </summary>
-public class FWAutoCompleteBox : AutoCompleteBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
-/// FluentJalium RichTextBox control.
-/// </summary>
-public class FWRichTextBox : RichTextBox, IFluentJaliumControl
-{
-}
-
-/// <summary>
 /// FluentJalium CheckBox control.
 /// </summary>
 public class FWCheckBox : CheckBox, IFluentJaliumControl
